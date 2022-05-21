32ef9db1a22cf3cca1a475c54d321bc258038a2f
Eric Sampaio <erick.gtn.xp@gmail.com>
2022-05-21 12:39:22 -0300

    manipulando elementos no DOM

--- DOM e Eventos/Manipulando_DOM/phones.js
+++ DOM e Eventos/Manipulando_DOM/phones.js
@@ -1,7 +1,15 @@
 function addNewPhone(){
     const phoneForm = document.querySelector("form#phones")
-    const newPhone = phoneForm.children(true)
+    const newPhone = phoneForm.children[0].cloneNode(true)
     const phonePosition = phoneForm.children.length + 1
     newPhone.querySelector("label").innerText = "Telefone " + phonePosition + ": "
     phoneForm.appendChild(newPhone)
+}
+function printPhones(){
+    let mensagem = ""
+    const phones = document.querySelectorAll("input[name='phone']")
+    phones.forEach((phone,index)=>{
+        mensagem +="Telefone " +(index + 1) + ": " +phone.value+"\n"
+    })
+    alert(mensagem)
 }
