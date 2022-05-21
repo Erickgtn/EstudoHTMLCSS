36ec073cccd0788e54aad5a17c2e75116534ad14
Eric Sampaio <erick.gtn.xp@gmail.com>
2022-05-21 09:39:34 -0300

    Eventos com JS e HTML

---
+++ DOM e Eventos/eventos.js
@@ -0,0 +1,15 @@
+function clikMe(){
+    console.log("Fui clicado")
+}
+function focusMe(){
+    console.log("fui focado")
+}
+function blurMe(){
+    console.log("fui desforcado")
+}
+function changeMe(){
+    console.log("fui alterado")
+}
+function overMe(){
+    console.log("Mouse aqui")
+}
